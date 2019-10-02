f0c07bbeef2c6776b76e90db4fe0aedc510638e5
Sudhakara7272 <martalareddy1991@gmail.com>
2019-10-02 16:13:02 +0530

    updated testng xml file

--- src/main/java/com/qa/BankApplications/listeners/ExtentReportListeners.java
+++ src/main/java/com/qa/BankApplications/listeners/ExtentReportListeners.java
@@ -1,5 +1,10 @@
 package com.qa.BankApplications.listeners;
 
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestNGListener;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -8,20 +13,39 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentAventReporter;
 
-public class ExtentReportListeners {
-	
-	ExtentReports report; // will help you to generate your report
-	ExtentTest logger; // loggin report.. Failed abd passed test cases
-	
-	
-	
-	@BeforeTest
-	public void launch() {
-		//generate extent report
+public class ExtentReportListeners implements ITestListener {
+
+	public void onFinish(ITestContext arg0) {
 		
 	}
-	@Test
-	public void passedTestCase() {
+
+	public void onStart(ITestContext arg0) {
 		
 	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+		
+	}
+
+	public void onTestFailure(ITestResult Result) {
+		System.out.println("Testcases failedand details are "+Result.getName());
+		
+	}
+
+	public void onTestSkipped(ITestResult Result) {
+		System.out.println("Testcases skipped and details are "+Result.getName());
+		
+	}
+
+	public void onTestStart(ITestResult Result) {
+		System.out.println("Testcases teststart and details are "+Result.getName());
+		
+	}
+
+	public void onTestSuccess(ITestResult Result) {
+		System.out.println("Testcases success and details are "+Result.getName());
+		
+	}
+	
+	
 }
